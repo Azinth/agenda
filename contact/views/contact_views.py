29eb86dc84759794a0cb36cbc3535f16ac62c93c
Azinth <gsv.jesus@gmail.com>
2024-01-19 18:12:56 -0300

    title adjustment and partials of head and header added

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -11,7 +11,8 @@ def index(request):
         .filter(show=True)\
         .order_by('-id')[0:10]
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -28,9 +29,12 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    
+    site_title = f'{single_contact.first_name} {single_contact.last_name} -  '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
