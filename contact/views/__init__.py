cdb0f53372a240b02d7e4595ff1c8cacb9db6171
Azinth <gsv.jesus@gmail.com>
2024-01-23 15:16:56 -0300

    view contact_form created and create url added

--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
 
 from .contact_views import *
+from .contact_form import *
+
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,5 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
@@ -68,12 +67,10 @@ def search(request):
 
         )\
         .order_by('-id')
-        
+
     paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-
-        
 
     context = {
         'page_obj': page_obj,
