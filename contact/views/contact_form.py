f89a015fc654c61685a871ffde35ddc5907a21f8
Azinth <gsv.jesus@gmail.com>
2024-01-23 17:28:41 -0300

    message error in fields added

--- contact/views/contact_form.py
+++ contact/views/contact_form.py
@@ -1,8 +1,10 @@
+from typing import Any
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django import forms
+from django.core.exceptions import ValidationError
 from contact.models import Contact
 
 
@@ -12,6 +14,25 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone',
         )
+        
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = self.cleaned_data
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )   
+        self.add_error(
+            'last_name',
+            ValidationError(
+                'Mensagem de erro2',
+                code='invalid'
+            )
+            
+        )
+        return super().clean()
 
 
 def create(request):
