0e5290cc4f5351ff6a56d316dcfd97113c948535
Azinth <gsv.jesus@gmail.com>
2024-01-23 14:59:50 -0300

    pagination implemented

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
@@ -10,9 +11,14 @@ def index(request):
 
     contacts = Contact.objects\
         .filter(show=True)\
-        .order_by('-id')[0:10]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -62,9 +68,15 @@ def search(request):
 
         )\
         .order_by('-id')
+        
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+        
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
