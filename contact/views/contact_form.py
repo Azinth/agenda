290204ffaee6e67f5a2fff87d58c7040c37832a8
Azinth <gsv.jesus@gmail.com>
2024-01-23 16:48:06 -0300

    fields in form added from model

--- contact/views/contact_form.py
+++ contact/views/contact_form.py
@@ -2,18 +2,35 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
-
+from django import forms
 from contact.models import Contact
 
 
-# Create your views here.
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+
 def create(request):
-
+    
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+        
     context = {
-
-    }
+            'form': ContactForm()
+        }
     return render(
-        request,
-        'contact/create.html',
-        context
-    )
+            request,
+            'contact/create.html',
+            context
+        )
