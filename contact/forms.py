be9446a6121ae14299ecf8adf0be76243fbb8044
Azinth <gsv.jesus@gmail.com>
2024-01-23 19:48:03 -0300

    initial validation of fields

--- contact/forms.py
+++ contact/forms.py
@@ -9,7 +9,6 @@ class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'class': "classe-a classe-b",
                 "placeholder": 'escreva aqui',
             }
         ),
@@ -40,20 +39,29 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro2',
-                code='invalid'
-            )
-
-        )
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        
+        if first_name == last_name:
+            msg = ValidationError(
+                    'Primeiro nome não pode ser igual ao segundo',
+                    
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+        
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'AB':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Não digite AB neste campo',
+                    code='invalid'
+                )
+            )
+        return first_name
