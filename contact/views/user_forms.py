fa77c6fe64a30c584c6684a3daa3358b9202f490
Azinth <gsv.jesus@gmail.com>
2024-01-27 16:43:39 -0300

    message added when a user is created

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-from django.urls import is_valid_path
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 from contact.forms import RegisterForm
 
 
 def register(request):
     form = RegisterForm
-    
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
-    
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
     return render(
         request,
         'contact/register.html',
