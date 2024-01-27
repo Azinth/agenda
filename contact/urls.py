0a30cc243814959a487b89f61754ff194093b5a7
Azinth <gsv.jesus@gmail.com>
2024-01-27 17:20:43 -0300

    login and logout page added

--- contact/urls.py
+++ contact/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from contact import views
 
-
 app_name = 'contact'
 
 urlpatterns = [
@@ -14,9 +13,11 @@ urlpatterns = [
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
-    
     # user (CRU)
     path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+
 
 
 ]
